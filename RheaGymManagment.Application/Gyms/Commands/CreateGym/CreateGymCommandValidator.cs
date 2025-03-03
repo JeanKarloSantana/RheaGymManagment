42b5482f99fb0bb59eed55c889dd190d1f83f2a6
jeankarlo_santana@bhd.com.do <jeankarlo_santana@bhd.com.do>
2025-03-02 22:53:40 -0400

    affed fluent validation to create gym

---
+++ RheaGymManagment.Application/Gyms/Commands/CreateGym/CreateGymCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RheaGymManagment.Application.Gyms.Commands.CreateGym
+{
+    public class CreateGymCommandValidator : AbstractValidator<CreateGymCommand>
+    {
+        public CreateGymCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .MinimumLength(3).MaximumLength(100);
+        }
+    }
+}
