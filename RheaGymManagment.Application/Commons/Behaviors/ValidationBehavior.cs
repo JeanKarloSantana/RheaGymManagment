120032a342af46953ac2faeac1ccbf602ea45a04
jeankarlo_santana@bhd.com.do <jeankarlo_santana@bhd.com.do>
2025-03-08 23:55:06 -0400

    added validator behaviors

--- RheaGymManagment.Application/Commons/Behaviors/ValidationBehavior.cs
+++ RheaGymManagment.Application/Commons/Behaviors/ValidationBehavior.cs
@@ -10,20 +10,20 @@ using System.Threading.Tasks;
 
 namespace RheaGymManagment.Application.Commons.Behaviors
 {
-    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null) : IPipelineBehavior<TRequest, TResponse> 
         where TRequest : IRequest<TResponse>
         where TResponse : IErrorOr
 
     {
-        private readonly IValidator<TRequest> _validator;
-
-        public ValidationBehavior(IValidator<TRequest> validator)
-        {
-            _validator = validator;
-        }
+        private readonly IValidator<TRequest>? _validator = validator;
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
+            if (_validator is null) 
+            {
+                return await next();
+            }
+
             var validationResult = await _validator.ValidateAsync(request, cancellationToken);
 
             if(validationResult.IsValid)
